5600cc63edee0899586a6f35cab718a982b02b41
QuentinFayt <quentin.fayt.p@gmail.com>
2021-09-08 11:12:03 +0200

    editing canva

--- editing.php
+++ editing.php
@@ -21,7 +21,42 @@
         <h1>Edit messages</h1>
     </header>
     <main>
-
+        <table>
+            <!-- Title : first row table -->
+            <tr>
+                <th>Id</th>
+                <th>Pseudo</th>
+                <th>Email</th>
+                <th>Message</th>
+                <th>Date-Time</th>
+                <th>
+                    <!-- Edit -->
+                </th>
+                <th>
+                    <!-- Delete -->
+                </th>
+            </tr>
+            <!-- On va boucler sur chaque nouvelle ligne pour chaque message présent dans la base de données plutôt que d'afficher en dur X messages -->
+            <!-- Content rows -->
+            <tr>
+                <th>1</th>
+                <th>...</th>
+                <th>...</th>
+                <th>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur qui consequuntur autem molestiae veritatis hic reprehenderit, dolor magnam et esse iure vel quisquam error quas eum sunt pariatur dicta assumenda?</th><!-- First message in index.php -->
+                <th>...</th>
+                <th><button>Edit</button></th>
+                <th><button>Delete</button></th>
+            </tr>
+            <tr>
+                <th>2</th>
+                <th>...</th>
+                <th>...</th>
+                <th>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum itaque pariatur, est porro incidunt eligendi quo numquam expedita odio cumque beatae excepturi nesciunt voluptatibus placeat, omnis repellendus aspernatur architecto explicabo!</th> <!-- Second message in index.php -->
+                <th>...</th>
+                <th><button>Edit</button></th>
+                <th><button>Delete</button></th>
+            </tr>
+        </table>
     </main>
     <footer>
         <hr>
