1d2a078a63eac83f838310cf177fb806f31860e3
QuentinFayt <quentin.fayt.p@gmail.com>
2021-11-21 12:24:28 +0100

    config

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("HOST","localhost");
-define("USER","root");
-define("PWD","");
-define("NAME","guestbook");
-define("PORT",3306);
-require ("../modele/db_connect_messages.php");
-require ("../controller/log.php");
-require ("../controller/check_page.php");
